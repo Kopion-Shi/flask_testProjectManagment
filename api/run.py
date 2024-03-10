3b9ac5bce8d174350cf99e2d9440458d268fec8e
jack_shi <carl3633524461@outlook.com>
2024-03-10 10:36:58 +0800

    first commit

---
+++ api/run.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask
+from flask import request, send_file
+from flask_cors import CORS
+import json
+
+import settings
+
+app = Flask(__name__, static_folder="static")
+CORS(app, supports_credentials=True)
+
+
+@app.route("/api/hello")
+def hellp():
+    return "hello word"
+
+
+@app.route("/api/user/login", methods=["POST"])
+def login():
+    data = request.get_data()
+    js_data = json.loads(data)
+
+    if "username" in js_data and js_data["username"] == "admin":
+        result = {"code": 20000, "data": {"token": "admin-token"}}
+
+    else:
+        result = {"code": 60204, "message": "账号密码错误，请检查"}
+
+    return result
+
+
+@app.route("/api/user/info", methods=["GET"])
+def info():
+    # 获取GET中请求token参数值
+    token = request.args.get("token")
+    if token == "admin-token":
+
+        # basedir一般是在配置文件中
+        result_success = {
+            "code": 20000,
+            "data": {
+                "roles": ["admin"],
+                "introduction": "I am a super administrator",
+                "avatar": request.host_url + "static/avatar/1.gif",
+                "name": "Super Admin",
+            },
+        }
+        return result_success
+    else:
+        result_error = {"code": 60204, "message": "用户信息获取错误"}
+        return result_error
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
